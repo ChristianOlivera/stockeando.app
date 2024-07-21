72698e382a2cab9d8eabfe348df141bd36c781ae
ChristianOlivera <105079050+ChristianOlivera@users.noreply.github.com>
2024-07-21 16:57:24 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,13 +1,56 @@
 import express from 'express'
-import {pool} from './db.js'
+import { pool } from './db.js'
 import { PORT } from './config.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/ping', async (req, res) => {
-    const [result] = await pool.query(` SELECT "hello world" as RESULT`);
+    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
     res.json(result[0])
 })
+
+
+app.get('/products', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM products');
+    res.json(rows);
+});
+
+app.get('/products/:id', async (req, res) => {
+    const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [req.params.id]);
+    if (rows.length > 0) {
+        res.json(rows[0]);
+    } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+    }
+});
+
+app.post('/products', async (req, res) => {
+    const { name, category, stock, price, description, image } = req.body;
+    const [result] = await pool.query('INSERT INTO products (name, category, stock, price, description, image) VALUES (?, ?, ?, ?, ?, ?)', [name, category, stock, price, description, image]);
+    res.json({ id: result.insertId, name, category, stock, price, description, image });
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, category, stock, price, description, image } = req.body;
+    const [result] = await pool.query('UPDATE products SET name = ?, category = ?, stock = ?, price = ?, description = ?, image = ? WHERE id = ?', [name, category, stock, price, description, image, id]);
+    if (result.affectedRows > 0) {
+        res.json({ id, name, category, stock, price, description, image });
+    } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+    }
+});
+
+app.delete('/products/:id', async (req, res) => {
+    const [result] = await pool.query('DELETE FROM products WHERE id = ?', [req.params.id]);
+    if (result.affectedRows > 0) {
+        res.json({ message: 'Producto eliminado' });
+    } else {
+        res.status(404).json({ message: 'Producto no encontrado' });
+    }
+});
 
 app.listen(PORT)
 console.log('server on port', PORT)
