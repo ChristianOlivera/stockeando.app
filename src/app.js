f83d45968b8fed44b9a48aa298d4ec7ec095c204
ChristianOlivera <105079050+ChristianOlivera@users.noreply.github.com>
2024-07-23 00:36:35 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -10,48 +10,48 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/ping', async (req, res) => {
-    const [result] = await pool.query(`SELECT "hello world" as RESULT`);
+    const [result] = await pool.query(`SELECT "hello world" as RESULT`)
     res.json(result[0])
 })
 
 
 app.get('/products', async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM products');
-    res.json(rows);
-});
+    const [rows] = await pool.query('SELECT * FROM products')
+    res.json(rows)
+})
 
 app.get('/products/:id', async (req, res) => {
-    const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [req.params.id]);
+    const [rows] = await pool.query('SELECT * FROM products WHERE id = ?', [req.params.id])
     if (rows.length > 0) {
-        res.json(rows[0]);
+        res.json(rows[0])
     } else {
-        res.status(404).json({ message: 'Producto no encontrado' });
+        res.status(404).json({ message: 'Producto no encontrado' })
     }
-});
+})
 
 app.post('/products', async (req, res) => {
-    const { name, category, stock, price, description, image } = req.body;
-    const [result] = await pool.query('INSERT INTO products (name, category, stock, price, description, image) VALUES (?, ?, ?, ?, ?, ?)', [name, category, stock, price, description, image]);
-    res.json({ id: result.insertId, name, category, stock, price, description, image });
-});
+    const { name, category, stock, price, description, image } = req.body
+    const [result] = await pool.query('INSERT INTO products (name, category, stock, price, description, image) VALUES (?, ?, ?, ?, ?, ?)', [name, category, stock, price, description, image])
+    res.json({ id: result.insertId, name, category, stock, price, description, image })
+})
 
 app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, category, stock, price, description, image } = req.body;
-    const [result] = await pool.query('UPDATE products SET name = ?, category = ?, stock = ?, price = ?, description = ?, image = ? WHERE id = ?', [name, category, stock, price, description, image, id]);
+    const { id } = req.params
+    const { name, category, stock, price, description, image } = req.body
+    const [result] = await pool.query('UPDATE products SET name = ?, category = ?, stock = ?, price = ?, description = ?, image = ? WHERE id = ?', [name, category, stock, price, description, image, id])
     if (result.affectedRows > 0) {
-        res.json({ id, name, category, stock, price, description, image });
+        res.json({ id, name, category, stock, price, description, image })
     } else {
-        res.status(404).json({ message: 'Producto no encontrado' });
+        res.status(404).json({ message: 'Error al Editar el Producto' })
     }
-});
+})
 
 app.delete('/products/:id', async (req, res) => {
-    const [result] = await pool.query('DELETE FROM products WHERE id = ?', [req.params.id]);
+    const [result] = await pool.query('DELETE FROM products WHERE id = ?', [req.params.id])
     if (result.affectedRows > 0) {
-        res.json({ message: 'Producto eliminado' });
+        res.json({ message: 'Producto eliminado' })
     } else {
-        res.status(404).json({ message: 'Producto no encontrado' });
+        res.status(404).json({ message: 'Error al Eliminar el Producto' })
     }
 });
 
